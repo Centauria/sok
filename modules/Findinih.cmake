7af774c2b3885b70643cac371be78b46b735a356
Centauria <centauriavc@gmail.com>
2022-04-07 22:13:39 +0800

    using INI configuration; supporting multi-monitor

---
+++ modules/Findinih.cmake
@@ -0,0 +1,12 @@
+set(src_dir ${CMAKE_SOURCE_DIR}/inih)
+
+set(src_test INIReaderTest.cpp)
+set(headers INIReader.h)
+
+list(TRANSFORM src_test PREPEND ${src_dir}/)
+list(TRANSFORM headers PREPEND ${src_dir}/)
+
+add_library(inih INTERFACE)
+target_include_directories(inih INTERFACE ${src_dir})
+
+mark_as_advanced(${src_dir})
